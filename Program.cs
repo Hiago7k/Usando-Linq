24675551f8c79151dea326d38f488689c7ffc1e8
Hiago 7k <hiago7k@gmail.com>
2026-03-18 20:27:44 -0300

    manipulando nossa APi de livros com LINQ

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using System.Linq;
 
 
-List<int> listaunica = new List<int> 
+//List<int> listaunica = new List<int> 
+//{
+//    1, 2, 3, 4, 5, 6
+//};
+
+
+//var listaComNumeroUnico = listaunica.Where(listaunica => listaunica.Equals(5)).ToList();
+
+//foreach (var numero in listaComNumeroUnico) 
+//{
+//    Console.WriteLine($"Numero escolhido: {numero}");
+//}
+
+// Desafio 2
+
+List<string> livros = new List<string>
 {
-    1, 2, 3, 4, 5, 6
+    "Dom Casmurro, Machado de Assis, 1899",
+    "1984, George Orwell, 1949",
+    "O Pequeno Principem, Antonio de Saint-Exupery, 1943",
+    "A Revolucao dos Bichos, George Orwell, 1945",
+    "Harry Potter e a Pedra Filosofal, J.k Rowling, 1997"
 };
-
-
-var listaComNumeroUnico = listaunica.Where(listaunica => listaunica.Equals(2)).ToList();
-
-
-foreach (var numero in listaComNumeroUnico) 
-{
-    Console.WriteLine($"Numero escolhido: {numero}");
-}
